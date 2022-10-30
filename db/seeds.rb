a0b73bd49a2dcdebb7e45b2c430f776416501caa
Astika Luintel <astild2020@gmail.com>
2022-10-30 11:32:52 -0400

    commit tree 44b3ecb47e9615bf802c8784d85a172ded26347b

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Student.create(first_name: "John", last_name: "Doe", grade: 10 )
+Student.create(first_name: "Sony", last_name: "mony", grade: 1 )
+Student.create(first_name: "Jow", last_name: "Moe", grade: 5)
+Student.create(first_name: "Moreey", last_name: "Mosy", grade: 17)
